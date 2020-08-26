a57123f9a357fc6201f0f68632b31d1856aab0cb
김정근 <gogoogjg@tripstore.kr>
2020-08-26 11:12:05 +0900

    initialize project

---
+++ src/test/java/me/kjgleh/future/FutureApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kjgleh.future;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
